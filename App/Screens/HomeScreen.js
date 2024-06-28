360d7805c962df77cf235f20e407bc424c205f82
Saif Salem <saifsalemmail@gmail.com>
2024-06-28 12:30:19 +0300

    sticky header

--- App/Screens/HomeScreen.js
+++ App/Screens/HomeScreen.js
@@ -8,7 +8,8 @@ import {
   Dimensions,
   Pressable,
   FlatList,
-  ActivityIndicator 
+  ActivityIndicator,
+  Animated,
 } from 'react-native';
 import Header from '../Components/HomeScreen/Header';
 import StoriesCard from '../Components/HomeScreen/storiesCard';
@@ -21,7 +22,7 @@ import {useNavigation} from '@react-navigation/native';
 import {getAllLessons, getLessonById} from '../Services/LessonServices';
 import {levels} from '../Utils/constants';
 import {useStateValue} from '../store/contextStore/StateContext';
-import { useSelector } from 'react-redux';
+import {useSelector} from 'react-redux';
 
 const {width, height} = Dimensions.get('window');
 
@@ -31,7 +32,7 @@ export default function HomeScreen() {
   const [activeTab, setActiveTab] = useState(0);
   const {state, dispatch: contextDispatch} = useStateValue();
   const [hideLearned, setHideLearned] = useState(false);
-  const learnedLessons= useSelector(state=> state.storyReducer.learned)
+  const learnedLessons = useSelector(state => state.storyReducer.learned);
   const [loading, setLoading] = useState(true);
   const tabs = [
     levels.A1,
@@ -49,14 +50,12 @@ export default function HomeScreen() {
   ];
 
   useEffect(() => {
-   
-    getAllLessons().then(resp => setLessons(resp),
-    
-    setLoading(false)
+    getAllLessons().then(
+      resp => setLessons(resp),
 
-  );
-  }
-  , []);
+      setLoading(false),
+    );
+  }, []);
   // useEffect(()=>{
   //   if(hideLearned){
   //     lessons.filter
@@ -71,7 +70,6 @@ export default function HomeScreen() {
 
   const HorizontalFlatList = ({lessons}) => {
     return (
-     
       <FlatList
         horizontal
         data={lessons}
@@ -101,167 +99,210 @@ export default function HomeScreen() {
         }
       />
     );
-    
   };
-  return (
-   
-       
-    
-    <ScrollView style={styles.mainStyles}>
-    
-      <Header />
-      { loading? (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center',paddingBottom:height*0.2,width:width,height:height}} >
 
-      <ActivityIndicator  size="large" color="#42BB7E" style={{
-
-      flex: 1,
-
-      justifyContent: 'center',
-
-      alignItems: 'center',
-
-      transform: [{ scale: 2 }] // increase the size
-
-        }}  />
-      </View>
-
-      ):(
-        <View>
-        <Text style={{padding: 10, color: 'black', fontFamily: 'outfit'}}></Text>
-        <View
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          gap: 7,
-          justifyContent: 'flex-end',
-          right: '1%',
-        }}>
-        <View
-          style={{
-            display: 'flex',
-
-            flexDirection: 'row',
-            gap: 7,
-            justifyContent: 'flex-end',
-            right: '1%',
-            borderRadius: 6,
-            padding: '1%',
-          }}>
-          <Text
-            style={{
-              fontFamily: 'outfit',
-              fontSize: 17,
-              color: '#333',
-            }}>
-            قصص مجانية
-          </Text>
-          <Ionicons name="gift-outline" size={19} color="black" />
-        </View>
-      </View>
-      <View style={{width: width, padding: 10, justifyContent: 'center'}}>
-        <HorizontalFlatList lessons={lessons} />
-      </View>
-
-      <View
-        style={{
-          display: 'flex',
-
-          flexDirection: 'row',
-          gap: 10,
-          justifyContent: 'flex-end',
-
-          borderRadius: '',
-          padding: '5%',
-          // left: '3%',
-        }}>
-        <View style={{paddingRight: width * 0.28}}>
-          <TouchableOpacity style={hideLearned? styles.activeLearned : styles.hideButton}
-          onPress={()=>setHideLearned(!hideLearned)}>
+  const tabsList = () => {
+    return (
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.tabsContentContainer}>
+        {tabs.map((tab, index) => (
+          <TouchableOpacity
+            key={index}
+            style={[styles.tab, activeTab === index && styles.activeTab]}
+            onPress={() => setActiveTab(index)}>
             <Text
-              style={{fontFamily: 'outfit', fontSize: 17, color: '#33333395'}}>
-              <Ionicons
-                name="checkmark-circle-outline"
-                size={18}
-                color="#33333395"
-              />
-              اخفاء ما تعلمت
+              style={
+                activeTab === index ? styles.tabTextActive : styles.tabText
+              }>
+              {tab.text}
             </Text>
           </TouchableOpacity>
-        </View>
-        <Text style={{fontFamily: 'outfit', fontSize: 17, color: 'black'}}>
-          بحث بالمستوي : كل
-        </Text>
-      </View>
-
-      <View style={styles.tabsContainer}>
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={styles.tabsContentContainer}>
-          {tabs.map((tab, index) => (
-            <TouchableOpacity
-              key={index}
-              style={[styles.tab, activeTab === index && styles.activeTab]}
-              onPress={() => setActiveTab(index)}>
-              <Text
-                style={
-                  activeTab === index ? styles.tabTextActive : styles.tabText
-                }>
-                {tab.text}
-              </Text>
-            </TouchableOpacity>
-          ))}
-        </ScrollView>
-      </View>
-      <ScrollView
-        horizontal={false}
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={{paddingHorizontal: '1%', gap: 5}}>
-        {lessons?.length > 0 &&
-          lessons.filter(f=> f.level == tabs?.[activeTab]?.text).filter(f=>{
-            if(hideLearned)
-              if(!learnedLessons){
-                learnedLessons=[]
-              }
-              else{
-
-                return !learnedLessons.some(s=> s == f?.id)
-              }
-            return true
-              }).map(
-            (lesson, index) =>
-              lesson?.paid && (
-                <Pressable
-                  key={index}
-                  onPress={() =>
-                    handleOnPress(
-                      lesson?.id,
-                      'data:image/png;base64,' + lesson?.image,
-                    )
-                  }>
-                  <View>
-                    <LevelsCard
-                      title={lesson?.title}
-                      description={lesson?.description}
-                      image={'data:image/png;base64,' + lesson?.image}
-                      key={index}
-                      onPress={() =>
-                        handleOnPress(
-                          lesson?.id,
-                          'data:image/png;base64,' + lesson?.image,
-                        )
-                      }
-                    />
-                  </View>
-                </Pressable>
-              ),
-          )}
+        ))}
       </ScrollView>
-      </View>
-    )}
-    </ScrollView>
-    
+    );
+  };
+  const whenToStick = height * 0.65;
+  const scrollY = new Animated.Value(0);
+  const stickyTop = scrollY.interpolate({
+    outputRange: [-height * 0.09, 0],
+    inputRange: [whenToStick, whenToStick + 1],
+    extrapolate: 'clamp',
+  });
+
+  return (
+    <View>
+      <ScrollView
+        style={styles.mainStyles}
+        onScroll={Animated.event(
+          [{nativeEvent: {contentOffset: {y: scrollY}}}],
+          {
+            useNativeDriver: false,
+          },
+        )}>
+        {loading ? (
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              paddingBottom: height * 0.2,
+              width: width,
+              height: height,
+            }}>
+            <ActivityIndicator
+              size="large"
+              color="#42BB7E"
+              style={{
+                flex: 1,
+
+                justifyContent: 'center',
+
+                alignItems: 'center',
+
+                transform: [{scale: 2}], // increase the size
+              }}
+            />
+          </View>
+        ) : (
+          <View>
+            <Header />
+            <Text
+              style={{
+                padding: 10,
+                color: 'black',
+                fontFamily: 'outfit',
+              }}></Text>
+            <View
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                gap: 7,
+                justifyContent: 'flex-end',
+                right: '1%',
+              }}>
+              <View
+                style={{
+                  display: 'flex',
+
+                  flexDirection: 'row',
+                  gap: 7,
+                  justifyContent: 'flex-end',
+                  right: '1%',
+                  borderRadius: 6,
+                  padding: '1%',
+                }}>
+                <Text
+                  style={{
+                    fontFamily: 'outfit',
+                    fontSize: 17,
+                    color: '#333',
+                  }}>
+                  قصص مجانية
+                </Text>
+                <Ionicons name="gift-outline" size={19} color="black" />
+              </View>
+            </View>
+            <View style={{width: width, padding: 10, justifyContent: 'center'}}>
+              <HorizontalFlatList lessons={lessons} />
+            </View>
+
+            <View
+              style={{
+                display: 'flex',
+
+                flexDirection: 'row',
+                gap: 10,
+                justifyContent: 'flex-end',
+
+                borderRadius: '',
+                padding: '5%',
+                // left: '3%',
+              }}>
+              <View style={{paddingRight: width * 0.28}}>
+                <TouchableOpacity
+                  style={hideLearned ? styles.activeLearned : styles.hideButton}
+                  onPress={() => setHideLearned(!hideLearned)}>
+                  <Text
+                    style={{
+                      fontFamily: 'outfit',
+                      fontSize: 17,
+                      color: '#33333395',
+                    }}>
+                    <Ionicons
+                      name="checkmark-circle-outline"
+                      size={18}
+                      color="#33333395"
+                    />
+                    اخفاء ما تعلمت
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <Text
+                style={{fontFamily: 'outfit', fontSize: 17, color: 'black'}}>
+                بحث بالمستوي : كل
+              </Text>
+            </View>
+
+            <View style={styles.tabsContainer}>{tabsList()}</View>
+
+            <ScrollView
+              horizontal={false}
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={{paddingHorizontal: '1%', gap: 5}}>
+              {lessons?.length > 0 &&
+                lessons
+                  .filter(f => f.level == tabs?.[activeTab]?.text)
+                  .filter(f => {
+                    if (hideLearned)
+                      if (!learnedLessons) {
+                        learnedLessons = [];
+                      } else {
+                        return !learnedLessons.some(s => s == f?.id);
+                      }
+                    return true;
+                  })
+                  .map(
+                    (lesson, index) =>
+                      lesson?.paid && (
+                        <Pressable
+                          key={index}
+                          onPress={() =>
+                            handleOnPress(
+                              lesson?.id,
+                              'data:image/png;base64,' + lesson?.image,
+                            )
+                          }>
+                          <View>
+                            <LevelsCard
+                              title={lesson?.title}
+                              description={lesson?.description}
+                              image={'data:image/png;base64,' + lesson?.image}
+                              key={index}
+                              onPress={() =>
+                                handleOnPress(
+                                  lesson?.id,
+                                  'data:image/png;base64,' + lesson?.image,
+                                )
+                              }
+                            />
+                          </View>
+                        </Pressable>
+                      ),
+                  )}
+            </ScrollView>
+          </View>
+        )}
+      </ScrollView>
+      <Animated.View
+        style={[
+          styles.animatedTabsContainer,
+          {top: stickyTop},
+        ]}>
+        {tabsList()}
+      </Animated.View>
+    </View>
   );
 }
 
@@ -269,7 +310,23 @@ const styles = StyleSheet.create({
   mainStyles: {
     backgroundColor: '#f5f5f5',
   },
+  animatedTabsContainer: {
+    alignItems: 'center',
+    paddingLeft: width * 0.07,
+    paddingRight: width * 0.02,
+    paddingTop: height * 0.02,
+    paddingBottom: height * 0.01,
+    width: width,
+    position: 'absolute',
+    top: -height * 0.09,
+    height: height * 0.09,
+    opacity: 1,
+    left: 0,
+    zIndex: 999,
+    backgroundColor: '#f5f5f5',
+  },
   tabsContainer: {
+    height: height * 0.09,
     alignItems: 'center',
     paddingLeft: width * 0.07,
     paddingRight: width * 0.02,
