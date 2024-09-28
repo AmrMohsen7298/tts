e297342050aff647f6e8ef24f5d544d005a2edfc
Saif Salem <saifsalemmail@gmail.com>
2024-09-28 18:19:50 +0300

    fix urls

--- App/Services/LessonServices.js
+++ App/Services/LessonServices.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getAllLessons = async() =>{
-    return await axios.get('https://tts.eliteacademyeg.com/api/tutorials',{method: 'GET',
+    return await axios.get('https://bel-arabi.com/api/tutorials',{method: 'GET',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
@@ -14,7 +14,7 @@ export const getAllLessons = async() =>{
 }
 
 export const getStoryAudio = async(storyId)=>{
-    return await fetch(`https://tts.eliteacademyeg.com/tts/paragraph/${storyId}`)
+    return await fetch(`https://bel-arabi.com/tts/paragraph/${storyId}`)
     .then( response => response.blob())
 
 
@@ -23,7 +23,7 @@ export const getStoryAudio = async(storyId)=>{
     });
 }
 export const getAudioTimePoints = async(storyId)=>{
-    return await axios.get(`https://tts.eliteacademyeg.com/tts/paragraph/timepoint/${storyId}`,{method: 'GET',
+    return await axios.get(`https://bel-arabi.com/tts/paragraph/timepoint/${storyId}`,{method: 'GET',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
@@ -34,7 +34,7 @@ export const getAudioTimePoints = async(storyId)=>{
     })
 }
 export const getLessonById = async(lessonId)=>{
-    return await axios.get(`https://tts.eliteacademyeg.com/api/tutorials/${lessonId}`,{method: 'GET',
+    return await axios.get(`https://bel-arabi.com/api/tutorials/${lessonId}`,{method: 'GET',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
@@ -47,7 +47,7 @@ export const getLessonById = async(lessonId)=>{
 }
 export const getStoryById = async(lessonId)=>{
     console.log("getSTory", lessonId)
-    return await axios.get(`https://tts.eliteacademyeg.com/api/v1/story/tutorial/${lessonId}`)
+    return await axios.get(`https://bel-arabi.com/api/v1/story/tutorial/${lessonId}`)
     .then(resp=> resp.data)
     .catch(error=>{
         console.log("error",error)
@@ -56,7 +56,7 @@ export const getStoryById = async(lessonId)=>{
 }
 export const getquestionbyId = async(lessonId)=>{
     console.log("getquestionbyId", lessonId)
-    return await axios.get(`https://tts.eliteacademyeg.com/api/v1/quiz/tutorial/${lessonId}`)
+    return await axios.get(`https://bel-arabi.com/api/v1/quiz/tutorial/${lessonId}`)
     .then(resp=> resp.data)
     .catch(error=>{
         console.log("error",error)
@@ -65,7 +65,7 @@ export const getquestionbyId = async(lessonId)=>{
 }
 export const getKeywordsbyTutorialId = async(lessonId)=>{
     console.log("getkeywordsbyId", lessonId)
-    return await axios.get(`https://tts.eliteacademyeg.com/keywords/getByTutorial/${lessonId}`)
+    return await axios.get(`https://bel-arabi.com/keywords/getByTutorial/${lessonId}`)
     .then(resp=> resp.data)
     .catch(error=>{
         console.log("error",error)
@@ -75,7 +75,7 @@ export const getKeywordsbyTutorialId = async(lessonId)=>{
 
 export const getGrammerByTutorialId = async(lessonId)=>{
     console.log("getgrammerByid", lessonId)
-    return await axios.get(`https://tts.eliteacademyeg.com/api/grammars/getTutorial/${lessonId}`)
+    return await axios.get(`https://bel-arabi.com/api/grammars/getTutorial/${lessonId}`)
     .then(resp=> resp.data)
     .catch(error=>{
         console.log("error",error)
@@ -83,7 +83,7 @@ export const getGrammerByTutorialId = async(lessonId)=>{
     )
 }
 export const getWordByText = async(word, tutorialId) =>{
-    return await  axios.get(`https://tts.eliteacademyeg.com/keywords/text`,{params:{
+    return await  axios.get(`https://bel-arabi.com/keywords/text`,{params:{
         word,
         tutorialId
     }})
@@ -93,14 +93,14 @@ export const getWordByText = async(word, tutorialId) =>{
     })
 }
 export const getQuizByTutorialId = async(lessonId) =>{
-    return await  axios.get(`https://tts.eliteacademyeg.com/api/v1/quiz/tutorial/${lessonId}`)
+    return await  axios.get(`https://bel-arabi.com/api/v1/quiz/tutorial/${lessonId}`)
     .then(res => res.data)
     .catch(error=>{
         console.log("error", error)
     })
 }
 export const getKeyWordsForTraining =async()=>{
-    return await  axios.get(`https://tts.eliteacademyeg.com/keywords/getForTraining`)
+    return await  axios.get(`https://bel-arabi.com/keywords/getForTraining`)
     .then(res => res.data)
     .catch(error=>{
         console.log("error", error)
