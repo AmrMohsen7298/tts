c562dd9daecd86721664ea50207c6cbcbdbc52d5
mohsencarlos <mohamedmmossad@gmail.com>
2024-11-16 14:13:08 +0200

    check

--- App/Screens/ProfileScreen.js
+++ App/Screens/ProfileScreen.js
@@ -301,27 +301,7 @@ const ProfileScreen = () => {
           <ReaderTrakerCard title="تتبع القراءة" storiesCount={0} />
         </View>
       </ScrollView>
-      {/*{!loggedInUser && (*/}
-      {/*  <View style={styles.loginButtonContainer}>*/}
-      {/*    <Pressable*/}
-      {/*      style={styles.button}*/}
-      {/*      onPress={() => setViewLoginOrSignupForm('login')}>*/}
-      {/*      <Text style={styles.buttonText}>تسجيل دخول</Text>*/}
-      {/*    </Pressable>*/}
-      {/*    <Pressable*/}
-      {/*      style={styles.button}*/}
-      {/*      onPress={() => setViewLoginOrSignupForm('signup')}>*/}
-      {/*      <Text style={styles.buttonText}>إنشاء حساب</Text>*/}
-      {/*            </Pressable>*/}
-      {/*            */}{/*<Pressable onPress={() => subscribeToApp()} style={styles.button}>*/}
-      {/*            */}{/*    <Text>*/}
-
-      {/*            */}{/*        subscribe*/}
-      {/*            */}{/*    </Text>*/}
-
-      {/*            */}{/*</Pressable>*/}
-      {/*  </View>*/}
-      {/*    )}*/}
+     
          
       <View style={styles.hairlineLeft}></View>
 
--- android/app/src/main/java/com/tts/MainActivity.kt
+++ android/app/src/main/java/com/tts/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "com.belarabi"
+  override fun getMainComponentName(): String = "belarabi"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
